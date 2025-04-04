0b43221e9aaa846e7228f5c287286712975507d0
Sergii <46484494+Space-researcher@users.noreply.github.com>
2025-04-04 15:42:57 +0300

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,34 @@
+'''
+Задание:
+Добавьте в файл conftest.py обработчик, который считывает из командной строки параметр language.
+Реализуйте в файле conftest.py логику запуска браузера с указанным языком пользователя.
+Браузер должен объявляться в фикстуре browser и передаваться в тест как параметр.
+'''
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",
+                     help="Select language: de, en, es, ru, uk or other")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("--language")  # Get language from CLI
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+# # На всякий случай - простая фикстура, которая возвращает браузер
+# @pytest.fixture
+# def browser():
+#     print("\nstart browser for test..")
+#     browser = webdriver.Chrome()
+#     yield browser
+#     # этот код выполнится после завершения теста
+#     print("\nquit browser..")
+#     browser.quit()
